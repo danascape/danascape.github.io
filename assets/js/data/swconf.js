5e1e0a1566136d6bc6752cef657a7f451b603abf
Saalim Quadri <danascape@gmail.com>
2025-08-14 01:06:21 +0530

    [CI]: Push GSoC draft pages
    Signed-off-by: Saalim Quadri <danascape@gmail.com>

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,1 +1,1 @@
-const swconf = { cacheName: 'chirpy-1755097740',resources: [ '/assets/css/jekyll-theme-chirpy.css', '/', '/categories/', '/tags/', '/archives/', '/about/', ], interceptor: {paths: [ ],urlPrefixes: [ ] }, purge: false };
+const swconf = { cacheName: 'chirpy-1755113751',resources: [ '/assets/css/jekyll-theme-chirpy.css', '/', '/categories/', '/tags/', '/archives/', '/about/', ], interceptor: {paths: [ ],urlPrefixes: [ ] }, purge: false };
