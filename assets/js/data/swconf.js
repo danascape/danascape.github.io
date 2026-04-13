36922e74d71e400f08d4ad97fdc761cbe1d9ce06
Saalim Quadri <danascape@gmail.com>
2026-04-13 15:04:41 +0530

    [Auto] Update
    Signed-off-by: Saalim Quadri <danascape@gmail.com>

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,1 +1,1 @@
-const swconf = { cacheName: 'chirpy-1774703075',resources: [ '/assets/css/jekyll-theme-chirpy.css', '/', '/categories/', '/tags/', '/archives/', '/about/', '/devices/', '/wiki/', '/faqs/', '/kernel/', ], interceptor: {paths: [ ],urlPrefixes: [ ] }, purge: false };
+const swconf = { cacheName: 'chirpy-1776072845',resources: [ '/assets/css/jekyll-theme-chirpy.css', '/', '/categories/', '/tags/', '/archives/', '/about/', '/devices/', '/wiki/', '/faqs/', '/kernel/', ], interceptor: {paths: [ ],urlPrefixes: [ ] }, purge: false };
